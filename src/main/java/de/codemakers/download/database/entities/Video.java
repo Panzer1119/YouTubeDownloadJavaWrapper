b3727c20a1ead3b70f4c5982b28bc4860952ebf5
Paul Hagedorn <panzer1119@gmail.com>
2020-01-02 12:03:39 +0100

    Created Video.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/download/database/entities/Video.java
@@ -0,0 +1,144 @@
+/*
+ *    Copyright 2019 - 2020 Paul Hagedorn (Panzer1119)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package de.codemakers.download.database.entities;
+
+import de.codemakers.download.Misc;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Video {
+    
+    public static final DateTimeFormatter DATE_TIME_FORMATTER_UPLOAD_DATE = DateTimeFormatter.ofPattern("yyyyMMdd");
+    
+    private String id;
+    private String uploader;
+    private String uploaderId;
+    private String title;
+    private String altTitle;
+    private long duration;
+    private LocalDate uploadDate;
+    
+    public Video() {
+        this(null, null, null, null, null, -1, null);
+    }
+    
+    public Video(String id, String uploader, String uploaderId, String title, String altTitle, long duration, LocalDate uploadDate) {
+        this.id = id;
+        this.uploader = uploader;
+        this.uploaderId = uploaderId;
+        this.title = title;
+        this.altTitle = altTitle;
+        this.duration = duration;
+        this.uploadDate = uploadDate;
+    }
+    
+    public String getId() {
+        return id;
+    }
+    
+    public Video setId(String id) {
+        this.id = id;
+        return this;
+    }
+    
+    public String getUploader() {
+        return uploader;
+    }
+    
+    public Video setUploader(String uploader) {
+        this.uploader = uploader;
+        return this;
+    }
+    
+    public String getUploaderId() {
+        return uploaderId;
+    }
+    
+    public Video setUploaderId(String uploaderId) {
+        this.uploaderId = uploaderId;
+        return this;
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+    
+    public Video setTitle(String title) {
+        this.title = title;
+        return this;
+    }
+    
+    public String getAltTitle() {
+        return altTitle;
+    }
+    
+    public Video setAltTitle(String altTitle) {
+        this.altTitle = altTitle;
+        return this;
+    }
+    
+    public String getDurationAsString() {
+        return Misc.durationToString(getDuration());
+    }
+    
+    public Duration getDuration() {
+        return Duration.ofMillis(getDurationAsMillis());
+    }
+    
+    public long getDurationAsMillis() {
+        return duration;
+    }
+    
+    public Video setDuration(String duration) {
+        return setDuration(Misc.stringToDuration(duration));
+    }
+    
+    public Video setDuration(Duration duration) {
+        return setDuration(duration.toMillis());
+    }
+    
+    public Video setDuration(long duration) {
+        this.duration = duration;
+        return this;
+    }
+    
+    public LocalDate getUploadDate() {
+        return uploadDate;
+    }
+    
+    public Video setUploadDate(long uploadDate) {
+        return setUploadDate("" + uploadDate);
+    }
+    
+    public Video setUploadDate(String uploadDate) {
+        return setUploadDate(LocalDate.parse(uploadDate, DATE_TIME_FORMATTER_UPLOAD_DATE));
+    }
+    
+    public Video setUploadDate(LocalDate uploadDate) {
+        this.uploadDate = uploadDate;
+        return this;
+    }
+    
+    @Override
+    public String toString() {
+        return "Video{" + "id='" + id + '\'' + ", uploader='" + uploader + '\'' + ", uploaderId='" + uploaderId + '\'' + ", title='" + title + '\'' + ", altTitle='" + altTitle + '\'' + ", duration=" + duration + ", uploadDate=" + uploadDate + '}';
+    }
+    
+}
