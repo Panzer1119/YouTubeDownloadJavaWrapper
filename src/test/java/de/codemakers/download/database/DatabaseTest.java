5d2f02e1b78a525497174f06390b3d910b31b6b4
Paul Hagedorn <panzer1119@gmail.com>
2020-01-02 13:42:17 +0100

    Created DatabaseTest.java
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/test/java/de/codemakers/download/database/DatabaseTest.java
@@ -0,0 +1,49 @@
+/*
+ *    Copyright 2019 - 2020 Paul Hagedorn (Panzer1119)
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
+package de.codemakers.download.database;
+
+import de.codemakers.base.Standard;
+import de.codemakers.base.logger.LogLevel;
+import de.codemakers.base.logger.Logger;
+import de.codemakers.download.database.entities.Video;
+import de.codemakers.io.file.AdvancedFile;
+
+import java.util.List;
+
+public class DatabaseTest {
+    
+    public static void main(String[] args) throws Exception {
+        Logger.getDefaultAdvancedLeveledLogger().setMinimumLogLevel(LogLevel.FINE);
+        final AdvancedFile databaseDirectory = new AdvancedFile(args[0]);
+        Logger.log("databaseDirectory=" + databaseDirectory);
+        final Database database = new Database(databaseDirectory);
+        Logger.log("database=" + database);
+        if (!database.start()) {
+            Logger.logError("Database didn't start?!");
+            return;
+        }
+        Standard.addShutdownHook(database::stop);
+        Logger.log("database=" + database);
+        final List<Video> videos_1 = database.getAllVideos();
+        //Logger.log("videos_1=" + videos_1);
+        Logger.log("videos_1:");
+        videos_1.forEach(System.out::println);
+        System.exit(0);
+    }
+    
+}
