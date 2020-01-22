8bd116cd13fe3951a7a586e78ad17c0523e79084
Paul Hagedorn <panzer1119@gmail.com>
2020-01-22 22:09:13 +0100

    Added "videoQueue" table to database_create_tables.sql Reformatted database_create_tables.sql
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/resources/de/codemakers/download/database_create_tables.sql
+++ src/main/resources/de/codemakers/download/database_create_tables.sql
@@ -1,43 +1,59 @@
-CREATE TABLE IF NOT EXISTS videos (
-    id VARCHAR(16) NOT NULL,
-    uploader VARCHAR(256),
+CREATE TABLE IF NOT EXISTS videos
+(
+    id         VARCHAR(16) NOT NULL,
+    uploader   VARCHAR(256),
     uploaderId VARCHAR(128),
-    title VARCHAR(1024),
-    altTitle VARCHAR(1024),
-    duration BIGINT,
+    title      VARCHAR(1024),
+    altTitle   VARCHAR(1024),
+    duration   BIGINT,
     uploadDate BIGINT,
     PRIMARY KEY (id)
 );
-CREATE TABLE IF NOT EXISTS mediaFiles (
-    videoId VARCHAR(16) NOT NULL,
-    file VARCHAR(1024) NOT NULL,
-    fileType VARCHAR (1) NOT NULL DEFAULT 'B',
-    format VARCHAR(128),
-    vcodec VARCHAR (32),
-    acodec VARCHAR (32),
-    width INT DEFAULT 0,
-    height INT DEFAULT 0,
-    fps INT DEFAULT 0,
-    asr INT DEFAULT 0,
+CREATE TABLE IF NOT EXISTS mediaFiles
+(
+    videoId  VARCHAR(16)   NOT NULL,
+    file     VARCHAR(1024) NOT NULL,
+    fileType VARCHAR(1)    NOT NULL DEFAULT 'B',
+    format   VARCHAR(128),
+    vcodec   VARCHAR(32),
+    acodec   VARCHAR(32),
+    width    INT                    DEFAULT 0,
+    height   INT                    DEFAULT 0,
+    fps      INT                    DEFAULT 0,
+    asr      INT                    DEFAULT 0,
     PRIMARY KEY (videoId, file)
 );
-CREATE TABLE IF NOT EXISTS extraFiles (
-    videoId VARCHAR(16) NOT NULL,
-    file VARCHAR(1024) NOT NULL,
-    fileType VARCHAR(16) NOT NULL,
+CREATE TABLE IF NOT EXISTS extraFiles
+(
+    videoId  VARCHAR(16)   NOT NULL,
+    file     VARCHAR(1024) NOT NULL,
+    fileType VARCHAR(16)   NOT NULL,
     PRIMARY KEY (videoId, file)
 );
-CREATE TABLE IF NOT EXISTS playlists (
-    id VARCHAR(64) NOT NULL,
-    title VARCHAR(256),
-    playlist VARCHAR(128),
-    uploader VARCHAR(256),
+CREATE TABLE IF NOT EXISTS playlists
+(
+    id         VARCHAR(64) NOT NULL,
+    title      VARCHAR(256),
+    playlist   VARCHAR(128),
+    uploader   VARCHAR(256),
     uploaderId VARCHAR(128),
     PRIMARY KEY (id)
 );
-CREATE TABLE IF NOT EXISTS playlistVideos (
-    playlistId VARCHAR(64) NOT NULL,
-    videoId VARCHAR(16) NOT NULL,
-    index INT DEFAULT -1,
+CREATE TABLE IF NOT EXISTS playlistVideos
+(
+    playlistId    VARCHAR(64) NOT NULL,
+    videoId       VARCHAR(16) NOT NULL,
+    playlistIndex INT DEFAULT -1,
     PRIMARY KEY (playlistId, videoId)
+);
+CREATE TABLE `videoQueue`
+(
+    `id`              int(11)     NOT NULL AUTO_INCREMENT,
+    `videoId`         varchar(64) NOT NULL,
+    `priority`        int(11)     NOT NULL DEFAULT 0,
+    `requested`       timestamp   NOT NULL DEFAULT current_timestamp(),
+    `arguments`       varchar(2048)        DEFAULT NULL,
+    `configFile`      varchar(512)         DEFAULT NULL,
+    `outputDirectory` varchar(512)         DEFAULT NULL,
+    PRIMARY KEY (`id`)
 );
